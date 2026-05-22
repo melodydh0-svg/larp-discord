4918833aa6745df27dacd24c62cde1e46a30aaf0
melodydh0-svg <melodydh0@gmail.com>
2026-05-22 18:08:42 +0200

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,37 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { patcher, common } from "@vendetta";
+import { storage } from "@vendetta/plugin";
+import { registerSettings } from "./Settings";
+
+export const pluginStorage = storage as {
+    spoofs: Record<string, { username: string, badges: number, memberSince: number }>
+};
+
+pluginStorage.spoofs ??= {};
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        // Intercept user profile data
+        this.unpatchUser = patcher.after("getUser", common.users, (args, ret) => {
+            if (!ret || !pluginStorage.spoofs[ret.id]) return ret;
+            const s = pluginStorage.spoofs[ret.id];
+            ret.username = s.username;
+            ret.publicFlags = s.badges;
+            return ret;
+        });
+
+        // Intercept account creation date
+        this.unpatchDate = patcher.after("default", common.components.MemberSince, (args, ret) => {
+            const userId = args[0]?.user?.id;
+            if (pluginStorage.spoofs[userId]) {
+                ret.props.children = new Date(pluginStorage.spoofs[userId].memberSince).toLocaleDateString();
+            }
+            return ret;
+        });
+
+        registerSettings();
     },
     onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+        this.unpatchUser?.();
+        this.unpatchDate?.();
+    }
+};
